07de0d47ffbc2ec7262bb6b1bf06e92c997a8823
lichangxu <845300336@qq.com>
2021-10-12 10:26:23 +0800

    搭建大概框架

---
+++ main.py
@@ -0,0 +1,11 @@
+# if __name__ == '__main__':
+#
+# 	# 创建handle类对象
+#     crawl_handle = crawl_handle.CrawlHandle()
+#     # 创建Crawler对象
+#     crawler = line_crawl.Crawler()
+#     # 网页版微博爬取
+#     crawl_handle.crawl_wb_and_write_excel(crawler)
+#
+
+if __name__ == '__main__':
---
+++ pc/crawl_handle.py
@@ -0,0 +1,33 @@
+import traceback
+
+
+class Crawl_handel:
+    """
+    爬取handle类，串联整个爬取和写入excel的过程
+    """
+
+    def crawl_wb_and_write_excel(self, crawler):
+        """
+        主方法：加载浏览器驱动、登录，爬取微博网页版中的数据，整理并存入指定excel
+        :param crawler:
+        :return:
+        """
+
+        try:
+            # 1 加载驱动
+            driver = self.create_driven(15)
+
+            # 2 加载首页面，并进行登录
+            cookies = self.login(driver)
+
+            # 3
+
+            # 4 关闭驱动
+            crawler.quiet_driver()
+
+            # 5 爬取所有数据后，写入excel文档
+            crawler.write_excel()
+
+        except Exception:
+            print("程序运行错误")
+            traceback.print_exc()
